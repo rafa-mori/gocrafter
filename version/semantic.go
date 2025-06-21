61be77d8fd473f8cfb8e8654b858a72f71c89a3c
Rafael Mori <faelmori@gmail.com>
2025-06-21 09:07:51 -0300

    feat: atualizar versão do CLI para v1.0.1 e corrigir URL de instalação do Go
    Signed-off-by: Rafael Mori <faelmori@gmail.com>

--- version/semantic.go
+++ version/semantic.go
@@ -38,7 +38,7 @@ func init() {
 var moduleAlias = "GoCrafter" // Default module alias, can be overridden by environment variable
 var moduleName = "gocrafter"  // Default module name, can be overridden by environment variable
 
-const currentVersionFallback = "v0.0.1"
+const currentVersionFallback = "v1.0.1"
 
 //go:embed CLI_VERSION
 var cliVersion string
